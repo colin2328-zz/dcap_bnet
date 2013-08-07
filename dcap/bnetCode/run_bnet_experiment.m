394dcd3518610f716809e6248a2c9589f2f23a09
colin <colin2328@gmail.com>
2013-08-07 12:15:30 -0400

    added more results

--- dcap/bnetCode/run_bnet_experiment.m
+++ dcap/bnetCode/run_bnet_experiment.m
@@ -3,18 +3,16 @@ function [ result ] = run_bnet_experiment(num_time_slice, lead_time_slice, min_t
 %  Trains a bnet
 tic;
 
-%% Result cell
-% add indices
-result = {K + 1};
-
-
+%% Initialize result cell
+% result cell contains one struct for each cross validation run.
+result = {K};
 
 %% Load data
 input_folder = ('data/');
 [ raw_data ] = load_data( input_file, input_folder );
 assert(mod(length(raw_data), num_time_slice) == 0)
 % number_of_bin = max(max(raw_data));
-[dropout_yes_bin, dropout_no_bin] = get_dropout_bin_values( raw_data);
+[dropout_yes_bin, ~] = get_dropout_bin_values( raw_data);
 
 
 %% Parse data by timeslice, features etc.
@@ -30,18 +28,31 @@ number_of_rows = countLines([input_folder input_file]);
 N = floor(number_of_rows / num_time_slice); % Size of total data set
 launch_matlabpool(number_of_threads);
 indices = pick_indices(N, K, number_to_train, number_to_test);
+for crossvalidation_number = 1:K
+   cell = {};
+   cell.indic
+   result{crossvalidation_number} = cell;
+end
+
 
 %% Train bnet
 launch_matlabpool(number_of_threads);
 
 parfor crossvalidation_number = 1:K
     data_train_idx = indices{crossvalidation_number}.data_train_idx;
+    data_test_idx = indices{crossvalidation_number}.data_test_idx;
     data_train = data(data_train_idx);
     data_train = cellfun(@(x) truncate_dropout(x, dropout_yes_bin), data_train, 'UniformOutput', false);
 
     [learnt_bnet, learnt_engine, loglik_trace, mapFeatureNameToId] = Build_DynamicBayesNetModel_with_hiddennodes(data_train, observable_node_support, 'hiddenVariableSupport', hidden_node_support, 'maximumNumberOfIterations', max_iterations, 'priorType','dirichlet');
+    
     crossval_result = {};
     crossval_result.learnt_bnet = learnt_bnet;
+    crossval_result.learnt_engine = learnt_engine;
+    crossval_result.loglik_trace = loglik_trace;
+    crossval_result.mapFeatureNameToId = mapFeatureNameToId;
+    crossval_result.data_train_idx = data_train_idx;
+    crossval_result.data_test_idx = data_test_idx;
     result{crossvalidation_number} = crossval_result;
 end
 
