1fb0413150ed37493dc4296e9fc940b8ed62ee1f
colin <colin2328@gmail.com>
2013-08-08 15:30:25 -0400

    restructured code

--- dcap/RunServer.py
+++ dcap/RunServer.py
@@ -17,7 +17,7 @@ def main():
     #Set up parser for input arguments, see http://docs.python.org/dev/library/argparse.html
     parser = argparse.ArgumentParser(description='Start a server that will listen for clients')
     parser.add_argument('-p','--port',type = int,default = 4444, dest = 'port', help='Specify on which port the server should listen, default is 4444')
-    parser.add_argument('-t','--tasks',type = str, default = './server/tasks/bnetTasks.txt', dest = 'pathToTasks', help='Specify where the tasks are locationed. Default is ./tasks.txt')
+    parser.add_argument('-t','--tasks',type = str, default = '../tasks/bnetTasks.txt', dest = 'pathToTasks', help='Specify where the tasks are locationed. Default is ./tasks.txt')
     parser.add_argument('-c','--completed',type = str, default = None, dest = 'pathToPreviouslyCompletedTasks', help = 'Specfy a list of subset of all tasks which have already been processed and should not be processed again. Default is None')
     parser.add_argument('-n','--name',type = str, default = '', dest = 'name', help = 'Specify a name for the task')
     args = parser.parse_args()
