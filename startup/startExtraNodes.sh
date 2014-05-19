601055173ef5108f853b92423e249bf730dbc294
colin <colin2328@gmail.com>
2014-05-19 19:40:10 -0400

    fix

--- startup/startExtraNodes.sh
+++ startup/startExtraNodes.sh
@@ -17,6 +17,12 @@ cd $DCAP_BNET_DIR
 
 #set environment for the euca2tools
 source $DCAP_BNET_DIR/ec2rc.sh #. is source, here source ec2rc script, make sure location is correct
+IP=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}') #gets my ip address
+
+echo -n "$IP" > $DCAP_BNET_DIR/serverIP.txt #puts ip address in info.txt
+echo "Our address is $IP" 
+write-mime-multipart -z -o $DCAP_BNET_DIR/multi.txt.gz $PART_HANDLER:text/part-handler $DCAP_BNET_DIR/serverIP.txt:text/plain $DCAP_BNET_DIR/$CERT.pem:text/plain $STARTUP_SCRIPT_DIR/clientBootstrap.sh:text/x-shellscript #creating an archive file for uploading to the cloud controller
+
 
 echo "Starting Instances"
 INSTANCE=$($EUCA_INITIALIZE_INSTANCES_CMD -k $CERT -n $NUM $AMI -t $TYPE -f $DCAP_BNET_DIR/multi.txt.gz | grep i- | cut -f 2)
